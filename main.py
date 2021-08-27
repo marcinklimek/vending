83aaac670e93701e505b959f173ea4f462b825e3
Marcin Klimek <contact@marcinklimek.pl>
2021-08-27 19:36:21 +0200

    [code] state machine #3

--- main.py
+++ main.py
@@ -16,22 +16,21 @@ class Controller:
         logging.debug("Start")
 
         self.state: int = const.STATE_IDLE
-
         self.flow: int = 0
         self.liquid: int = 0
         self.coins: int = 0
         self.led_time: float = 0.5
-
-        self.coins_queue = asyncio.Queue()
+        self.timeout_timer: int = const.CANCEl_TIMEOUT
 
         self.setup()
-
         self.reset()
 
     def reset(self):
         self.flow = 0
         self.liquid = 0
         self.coins = 0
+        self.led_time = 0.5
+        self.timeout_timer = const.CANCEl_TIMEOUT
 
     def setup(self) -> None:
 
@@ -70,8 +69,10 @@ class Controller:
         GPIO.add_event_detect(const.IN_COIN_TERMINAL, GPIO.RISING, callback=self.coin_callback)
 
     def flow_sensor_callback(self, channel: int) -> None:
+        self.reset_timer()
 
         self.flow += 1
+
         logging.debug("flow in = {}".format(self.flow))
 
     def button_callback(self, channel: int) -> None:
@@ -79,6 +80,21 @@ class Controller:
 
     def led(self, state: bool) -> None:
         GPIO.output(const.OUT_LED, state)
+
+    def reset_timer(self):
+        self.timeout_timer = const.CANCEl_TIMEOUT
+
+    async def timeout_task(self):
+        try:
+            while True:
+                await asyncio.sleep(1)
+
+                if self.timeout_timer > 0:
+                    self.timeout_timer -= 1
+                    logging.debug("timeout timer: {}".format(self.timeout_timer))
+
+        except asyncio.CancelledError:
+            self.reset_timer()
 
     async def led_task(self):
         try:
@@ -94,11 +110,12 @@ class Controller:
 
     def inc_liquid(self, amount: int):
 
-        self.liquid += self.liquid + amount * 100
-        logging.debug("liquid = {}".format(self.liquid))
+        self.liquid += amount * 100
+        logging.debug("liquid = {} ({})".format(self.liquid, amount))
 
     def coin_callback(self, channel: int) -> None:
-        logging.debug("coins: {}".format(channel))
+
+        self.reset_timer()
 
         if channel == const.IN_COIN_1:    # 1
             self.coins = self.coins + 1
@@ -113,10 +130,10 @@ class Controller:
             self.coins = self.coins + 1
             self.inc_liquid(1)
 
-        logging.debug("coins = {}".format(self.coins))
+        logging.debug("coins amount = {}".format(self.coins))
 
-    def enough_liquid(self) -> bool:
-        return (self.liquid - self.flow) > 0
+    def check(self) -> bool:
+        return (self.timeout_timer > 0) and (self.liquid - self.flow) > 0  # enough liquid and idle timer not timed out?
 
     def pump(self, state) -> None:
         GPIO.output(const.OUT_PUMP_1, state)
@@ -124,28 +141,32 @@ class Controller:
     def change_state(self, value):
         self.state = value
 
-    async def run(self, event_loop):
-
-        led_task = None
+    async def run(self, loop):
 
         while True:
 
             if self.state == const.STATE_IDLE:
-                if self.enough_liquid():
+                if self.check():
                     self.pump(True)
                     self.change_state(const.STATE_WORKING)
-                    led_task = event_loop.create_task(self.led_task())
+                    blink = loop.create_task(self.led_task())
+                    timeout = loop.create_task(self.timeout_task())
 
             if self.state == const.STATE_WORKING:
-                if not self.enough_liquid():
+                if not self.check():
                     self.pump(False)
                     self.reset()
                     self.change_state(const.STATE_IDLE)
 
-                    if not led_task == None:
-                        led_task.cancel()
+                    if blink is not None:
+                        blink.cancel()
+
+                    if timeout is not None:
+                        timeout.cancel()
 
             await asyncio.sleep(0.1)
+
+
 
 
 if __name__ == '__main__':
