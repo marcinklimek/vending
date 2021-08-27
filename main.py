4d24d8fd7eea147635d1adc2fa8e9e8464b4d34d
Marcin Klimek <contact@marcinklimek.pl>
2021-08-27 18:16:27 +0200

    towards the state machine

--- main.py
+++ main.py
@@ -3,84 +3,121 @@
 
 import RPi.GPIO as GPIO
 import asyncio
+import logging
+import constans as const
 
-GPIO06 = 31
-GPIO07 = 26
-GPIO08 = 24
-GPIO12 = 32
-GPIO13 = 33
-GPIO14 = 8
-GPIO15 = 10
-GPIO16 = 36
-GPIO18 = 12
-GPIO18 = 12
-GPIO19 = 35
-GPIO20 = 38
-GPIO23 = 16
-GPIO23 = 16
-GPIO24 = 18
-GPIO25 = 22
-GPIO26 = 37
-
-OUT_LED = GPIO20
-OUT_PUMP_1 = GPIO16
-OUT_PUMP_2 = GPIO12
-IN_FLOW_SENSOR = GPIO07
-
-IN_ENTER = GPIO06
-IN_DOWN = GPIO13
-IN_UP = GPIO19
-IN_CANCEL = GPIO26
-
-IN_START = GPIO24
-IN_STOP = GPIO25
+logging.basicConfig(level=logging.DEBUG)
 
 
-def flow_sensor_callback(channel):
-    print("[debug] flow in")
+class Controller:
 
+    def __init__(self):
+        self.flow: int = 0
+        self.liquid: int = 0
+        self.coins: int = 0
+        self.led_time: float = 0.5
 
-def buttons_callback(channel):
-    print("[debug] buttons: {}".format(channel))
+        self.coins_queue = asyncio.Queue()
 
+        self.setup()
 
-def setup():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setwarnings(True)
+        self.reset()
 
-    GPIO.setup(OUT_LED, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(OUT_PUMP_1, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(OUT_PUMP_2, GPIO.OUT, initial=GPIO.LOW)
+    def reset(self):
+        self.flow = 0
+        self.liquid = 0
+        self.coins = 0
 
-    GPIO.setup(IN_FLOW_SENSOR, GPIO.IN)
+    def setup(self) -> None:
 
-    GPIO.setup(IN_ENTER, GPIO.IN)
-    GPIO.setup(IN_UP, GPIO.IN)
-    GPIO.setup(IN_DOWN, GPIO.IN)
-    GPIO.setup(IN_CANCEL, GPIO.IN)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(True)
 
-    # events
-    GPIO.add_event_detect(IN_FLOW_SENSOR, GPIO.RISING, callback=flow_sensor_callback)
+        GPIO.setup(const.OUT_LED, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(const.OUT_PUMP_1, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(const.OUT_PUMP_2, GPIO.OUT, initial=GPIO.LOW)
 
-    GPIO.add_event_detect(IN_ENTER, GPIO.FALLING, callback=buttons_callback)
-    GPIO.add_event_detect(IN_UP, GPIO.FALLING, callback=buttons_callback)
-    GPIO.add_event_detect(IN_DOWN, GPIO.FALLING, callback=buttons_callback)
-    GPIO.add_event_detect(IN_CANCEL, GPIO.FALLING, callback=buttons_callback)
+        GPIO.setup(const.IN_FLOW_SENSOR, GPIO.IN)
 
+        GPIO.setup(const.IN_ENTER, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(const.IN_UP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(const.IN_DOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(const.IN_CANCEL, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(const.IN_START, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(const.IN_CANCEL, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-async def worker():
-    while True:
-        await asyncio.sleep(1)
-        print("First Worker Executed")
+        GPIO.setup(const.IN_COIN_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(const.IN_COIN_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(const.IN_COIN_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(const.IN_COIN_TERMINAL, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        # events
+        GPIO.add_event_detect(const.IN_FLOW_SENSOR, GPIO.RISING, callback=self.flow_sensor_callback)
+
+        GPIO.add_event_detect(const.IN_ENTER, GPIO.FALLING, callback=self.button_callback)
+        GPIO.add_event_detect(const.IN_UP, GPIO.FALLING, callback=self.button_callback)
+        GPIO.add_event_detect(const.IN_DOWN, GPIO.FALLING, callback=self.button_callback)
+        GPIO.add_event_detect(const.IN_CANCEL, GPIO.FALLING, callback=self.button_callback)
+
+        GPIO.add_event_detect(const.IN_COIN_1, GPIO.RISING, callback=self.coin_callback)
+        GPIO.add_event_detect(const.IN_COIN_2, GPIO.RISING, callback=self.coin_callback)
+        GPIO.add_event_detect(const.IN_COIN_3, GPIO.RISING, callback=self.coin_callback)
+        GPIO.add_event_detect(const.IN_COIN_TERMINAL, GPIO.RISING, callback=self.coin_callback)
+
+    def flow_sensor_callback(self, channel: int) -> None:
+
+        self.flow += 1
+        logging.debug("flow in = {}".format(self.flow))
+
+    def button_callback(self, channel: int) -> None:
+        logging.debug("button: {}".format(channel))
+
+    def led(self, state: bool) -> None:
+        GPIO.output(const.OUT_LED, state)
+
+    async def led_task(self):
+        try:
+            while True:
+                self.led(True)
+                await asyncio.sleep(self.led_time)
+
+                self.led(False)
+                await asyncio.sleep(self.led_time)
+
+        except asyncio.CancelledError:
+            self.led(False)
+
+    def coin_callback(self, channel: int) -> None:
+        logging.debug("coins: {}".format(channel))
+
+        if channel == const.IN_COIN_1:  # 1
+            self.coins = self.coins + 1
+        elif channel == const.IN_COIN_2:  # 2
+            self.coins = self.coins + 2
+        elif channel == const.IN_COIN_3:  # 5
+            self.coins = self.coins + 5
+
+        logging.debug("coins = {}".format(self.coins))
+
+    def run(self):
+
+        while True:
+
+            if self.coins > 0:
+                pass
+                # start pump
+            else:
+                pass
+                # stop pump
+
 
 if __name__ == '__main__':
 
-    setup()
+    ctrl = Controller()
 
     loop = asyncio.get_event_loop()
     try:
-        asyncio.ensure_future(worker())
-        loop.run_forever()
+        loop.run_until_complete(ctrl.run())
 
     except KeyboardInterrupt:
         pass
