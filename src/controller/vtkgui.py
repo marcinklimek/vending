ea543b10fdf5252d40372ad4fc65fd8f7b842009
Marcin Klimek <contact@marcinklimek.pl>
2022-01-13 15:54:58 +0100

    [code] tkinter gui

--- src/controller/vtkgui.py
+++ src/controller/vtkgui.py
@@ -2,9 +2,33 @@ import tkinter
 import time
 
 root = tkinter.Tk()
+root.geometry('1024x600')
+root.resizable(0, 0)
+root.resizable(False, False)
+root.resizable(width=False, height=False)
+#root.wm_attributes('-transparentcolor','black')
+
+canvas = tkinter.Canvas(
+    root,
+    width=1024,
+    height=600
+)
+canvas.pack()
+
+img = tkinter.PhotoImage(file='bin/bkg.png')
+canvas.create_image(
+    0,
+    0,
+    anchor=tkinter.NW,
+    image=img
+)
+
 time1 = ''
-clock = tkinter.Label(root, font=('times', 20, 'bold'), bg='green')
-clock.pack(fill=tkinter.BOTH, expand=1)
+
+title_text = canvas.create_text(560,  75, fill="white", font="Segoe 25 bold", anchor=tkinter.NW, text="Dystrybutor płynu")
+price_text = canvas.create_text(306, 545, fill="white", font="Segoe 25 bold", anchor=tkinter.NW, text="Cena jednego litra płynu: 2 PLN")
+start_text = canvas.create_text(856, 570, fill="white", font="Segoe 25 bold", anchor=tkinter.NW, text="START")
+company_text = canvas.create_text(10, 10, fill="white", font="Segoe 25 bold", anchor=tkinter.NW, text="FIRMA ABC")
 
 def tick():
     global time1
@@ -13,9 +37,11 @@ def tick():
 
     if time2 != time1:
         time1 = time2
-        clock.config(text=time2)
+
+        #canvas.itemconfig(mytext, text=time2)
 
     root.after(200, tick)
+
 
 tick()
 root.mainloop()
